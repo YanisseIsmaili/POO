160ff248c172efa718c3322ca5afa16aa8e7ca30
yaourt <yanisse.ismaili@ynov.com>
2023-02-06 12:55:16 +0100

    Avengers_Rasamblement

--- src/main/java/com/ynov/oop/ressource/Warehouse.java
+++ src/main/java/com/ynov/oop/ressource/Warehouse.java
@@ -1,5 +1,19 @@
 package com.ynov.oop.ressource;
 
 public class Warehouse {
-    
+    private Credits credits; 
+    private Plants plants;
+    private Steel steel;
+
+    public Warehouse(Credits c,Plants p,Steel s) {
+        this.credits = c;
+        this.plants = p;
+        this.steel = s;
+    }
+
+    public void produce(){
+    this.credits.produce();
+    this.plants.produce();
+    this.steel.produce();
+    }
 }
